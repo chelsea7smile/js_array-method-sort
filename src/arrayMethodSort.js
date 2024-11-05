6ae450b0ed9a40ea3f31d24564040b503ccf334a
Yevhenii Kucherenko <102630213+chelsea7smile@users.noreply.github.com>
2024-11-05 20:04:30 +0200

    Update arrayMethodSort.js

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,12 +5,12 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction) {
-    const comparison =
+    const toCompare =
       compareFunction || ((a, b) => (a.toString() > b.toString() ? 1 : -1));
 
     for (let i = 0; i < this.length - 1; i++) {
       for (let j = 0; j < this.length - 1 - i; j++) {
-        if (comparison(this[j], this[j + 1]) > 0) {
+        if (toCompare(this[j], this[j + 1]) > 0) {
           const temp = this[j];
 
           this[j] = this[j + 1];
